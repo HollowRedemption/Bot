52509dc76141f17e68a99fbc9b2f0e5a4275805e
HollowRedemption <80590775+HollowRedemption@users.noreply.github.com>
2024-09-06 12:27:52 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,9 +1,9 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 import subprocess
-import os
-
-TOKEN = '7107496073:AAEaoHj-MpmTwq2Ro-HG73utmB037Nywdt8'  # Remplace par le token de ton bot
+import tempfile
+TOKEN = os.getenv('TOKEN')  # Utilise la variable d'environnement
 
 async def download(update: Update, context: CallbackContext) -> None:
     if len(context.args) != 1:
@@ -12,7 +12,7 @@ async def download(update: Update, context: CallbackContext) -> None:
 
     url = context.args[0]
     chat_id = update.message.chat_id
-    video_path = 'C:/Users/SnifL/Videos/Captures'  # Dossier où les vidéos seront sauvegardées
+    video_path = tempfile.mkdtemp()  # Dossier où les vidéos seront sauvegardées
 
     if not os.path.exists(video_path):
         os.makedirs(video_path)
@@ -26,7 +26,6 @@ async def download(update: Update, context: CallbackContext) -> None:
         if video_file:
             video_file_path = os.path.join(video_path, video_file)
             await update.message.reply_document(document=open(video_file_path, 'rb'))
-
 
 
 def main() -> None:
